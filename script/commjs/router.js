5349b47164a158c8069335b2eae97725216cee28
lixueqing <lixueqing1127@gmail.com>
2017-07-05 17:50:30 +0800

    1

--- script/commjs/router.js
+++ script/commjs/router.js
@@ -1,13 +1,14 @@
 /**
  * 路由
  */
-var host = "http://192.168.0.159:7001/api/v1/";
+var host = "http://192.168.0.159:7001";
+var v="/api/v1/";
 
 var UrlRouter = {
     login: host + 'login',  //登陆接口
-    user: host + 'user',  //用户信息
-    catalog: host + 'catalog',//分类
-    collection: host + 'collection', //意见反馈
-    article: host + 'article', //
-    banner: host + 'banner',
+    user: host +v+ 'user',  //用户信息
+    catalog: host +v+ 'catalog',//分类
+    collection: host + v+'collection', //意见反馈
+    article: host +v+ 'article', //
+    banner: host + v+'banner',
 }
