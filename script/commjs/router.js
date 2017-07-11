9c0fb05eff051c3dc8a55e88d776fd1c7eae00d5
lixueqing <lixueqing1127@gmail.com>
2017-07-11 17:54:37 +0800

    login

--- script/commjs/router.js
+++ script/commjs/router.js
@@ -1,8 +1,8 @@
 /**
  * 路由
  */
-var host = "http://192.168.0.159:7001";
-var v="/api/v1/";
+var host = "http://192.168.0.159:7001/";
+var v="api/v1/";
 
 var UrlRouter = {
     login: host + 'login',  //登陆接口
