1e5477b331f5a98dfd6243a183f84409b2033028
lixueqing <lixueqing1127@gmail.com>
2017-07-21 15:42:15 +0800

    关于我们

--- script/commjs/router.js
+++ script/commjs/router.js
@@ -8,7 +8,7 @@ var UrlRouter = {
     login: host + 'login',  //登陆接口
     user: host +v+ 'user',  //用户信息
     catalog: host +v+ 'catalog',//分类
-    collection: host + v+'collection', //意见反馈
+    collection: host + v+'collection', //收藏
     article: host +v+ 'article', //
     banner: host + v+'banner',
     reply: host + v+'reply',
