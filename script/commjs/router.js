8aaf45fdfac3d1b12dcb66735e2d1aa0b6654bd3
jeremy16601 <lixueqing1127@gmail.com>
2017-11-06 17:42:43 +0800

    搜索

--- script/commjs/router.js
+++ script/commjs/router.js
@@ -3,16 +3,17 @@
  */
 var host = "http://api.bbaaoo.cn/";
 // var host = "http://192.168.0.160:7001/";
-var v="api/v1/";
+var v = "api/v1/";
 
 var UrlRouter = {
     login: host + 'login',  //登陆接口
-    user: host +v+ 'user',  //用户信息
-    catalog: host +v+ 'catalog',//分类
-    collection: host + v+'collection', //收藏
-    article: host +v+ 'article', //
-    banner: host + v+'banner',
-    reply: host + v+'reply',
-    feedback:host+v+'feedback',
-    uploadToken:host+'uploadToken',//七牛token
+    user: host + v + 'user',  //用户信息
+    catalog: host + v + 'catalog',//分类
+    collection: host + v + 'collection', //收藏
+    article: host + v + 'article', //
+    banner: host + v + 'banner',
+    reply: host + v + 'reply',
+    feedback: host + v + 'feedback',
+    uploadToken: host + 'uploadToken',//七牛token
+    search: host +v+ 'search', //搜索
 }
