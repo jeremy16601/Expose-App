0576d62f7b77accb96295a70fd042947e45896e6
jeremy16601 <lixueqing1127@gmail.com>
2018-02-28 09:45:47 +0800

    反馈功能

--- script/commjs/router.js
+++ script/commjs/router.js
@@ -1,19 +1,22 @@
 /**
  * 路由
  */
-var host = "http://api.bbaaoo.cn/";
-// var host = "http://192.168.0.160:7001/";
+// var host = "http://api.bbaaoo.cn/";
+var host = "http://192.168.0.157:7001/";
 var v = "api/v1/";
 
 var UrlRouter = {
-    login: host + 'login',  //登陆接口
-    user: host + v + 'user',  //用户信息
-    catalog: host + v + 'catalog',//分类
+    login: host + 'login', //登陆接口
+    login_new: host + 'v1/login', // 带token的接口
+    user: host + v + 'user', //用户信息
+    catalog: host + v + 'catalog', //分类
     collection: host + v + 'collection', //收藏
     article: host + v + 'article', //
     banner: host + v + 'banner',
     reply: host + v + 'reply',
     feedback: host + v + 'feedback',
-    uploadToken: host + 'uploadToken',//七牛token
-    search: host +v+ 'search', //搜索
+    feedback_Byuid: host + v + 'feedback_Byuid', //根据uid查询反馈列表
+    uploadToken: host + 'uploadToken', //七牛token
+    search: host + v + 'search', //搜索
+    errors: host + v + 'errors', //错误收集
 }
